45539fb0d33e4b01cfab31439499087796ff818d
BRADDTELES <braddteles@gmail.com>
2024-08-29 15:16:07 -0300

    Declarações de Tipos: final VS const

---
+++ Curso Flutter e Dart/Dart/tipoFinalVsConst.dart
@@ -0,0 +1,10 @@
+void main() {
+
+final DateTime canal2 = DateTime.now();
+// const canal3 = DateTime.now(); /* DateTime não aceita em constantes */
+const texto = "blablablablablabla";
+
+print(canal2);
+print(texto);
+
+}
