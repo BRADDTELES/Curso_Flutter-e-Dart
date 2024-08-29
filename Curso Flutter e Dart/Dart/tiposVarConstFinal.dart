aacd8d185658886f0694600c54fe33eb0d2affe9
BRADDTELES <braddteles@gmail.com>
2024-08-29 15:05:57 -0300

    Tipos: var, const e final. Tipos mutaveis e imutaveis

---
+++ Curso Flutter e Dart/Dart/tiposVarConstFinal.dart
@@ -0,0 +1,18 @@
+void main() {
+  // variáveis
+  // var const final
+
+  var canal = "WebDutras";
+  // canal = 2; /* Erro */
+  final canal2 = "WebDutras 2";
+  const canal3 = "WebDutras 3";
+  print(canal);
+  print(canal2);
+  print(canal3);
+
+  /* Mutavel e Imutavel */
+  canal = "WebDutras direrente";
+  // canal2 = "WebDutras 2 diferente"; /* Erro */
+  // canal3 = "WebDutras 3 diferente"; /* Erro */
+
+}
