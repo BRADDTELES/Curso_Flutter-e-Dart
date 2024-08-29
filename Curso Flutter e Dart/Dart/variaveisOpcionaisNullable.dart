c9e11f8d50c91168bec23e1c462598d3e7d6665f
BRADDTELES <braddteles@gmail.com>
2024-08-29 16:00:28 -0300

    Variáveis opcionais;nullable

---
+++ Curso Flutter e Dart/Dart/variaveisOpcionaisNullable.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  String? nome = null;
+  /* variaveis não permite o nulo */
+  // int preco = null;
+  // double promocao = null;
+  // bool isAdulto = null;
+
+  // nome = "Danillo";
+
+  print(nome);
+  print(nome?.length);
+
+  /* O const e final pode receber o nulo */
+}
