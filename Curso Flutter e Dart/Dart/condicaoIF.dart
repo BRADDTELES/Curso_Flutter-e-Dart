9ce58b5f309edf10f3df27c18de97d9cd91dcbeb
BRADDTELES <braddteles@gmail.com>
2024-08-30 22:44:28 -0300

    ft 2 Condições IF

--- Curso Flutter e Dart/Dart/condicaoIF.dart
+++ Curso Flutter e Dart/Dart/condicaoIF.dart
@@ -1,5 +1,6 @@
 bool isVip = false;
 int age = 15;
+String nome = "";
 
 void main() {
     // if
@@ -20,19 +21,26 @@ void main() {
     //     print("Tchau!");
     // }
 
-    isVip = !isVip;
+    // isVip = !isVip;
 
-    if(isVip || age >= 18) {
+    // if(isVip || age >= 18) {
         
-        if(isVip) {
-            print("conduzir a sala vip");
-        } else {
-            print("entrada permitida");
-        }
-    } else if ( age >= 65) {
-        print("vai para sala vip de 3º idade");
+    //     if(isVip) {
+    //         print("conduzir a sala vip");
+    //     } else {
+    //         print("entrada permitida");
+    //     }
+    // } else if ( age >= 65) {
+    //     print("vai para sala vip de 3º idade");
+    // } else {
+    //     print("tchau!");
+    // }
+
+    //isEmpty significa está vazio
+    if(nome.isEmpty) {
+        print('está vazio');
     } else {
-        print("tchau!");
+        print('não está vazio');
     }
-    
+
 }
