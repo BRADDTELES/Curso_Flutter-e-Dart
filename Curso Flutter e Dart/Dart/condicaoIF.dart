80fc28e98dece918707d52931df2815644b40fb4
BRADDTELES <braddteles@gmail.com>
2024-08-30 22:31:08 -0300

    Condições IF

---
+++ Curso Flutter e Dart/Dart/condicaoIF.dart
@@ -0,0 +1,22 @@
+bool isVip = false;
+int age = 15;
+
+void main() {
+    // if
+
+    // if (isVip) {
+    //     print("é VIP");
+    // } else {
+    //     print("Não é VIP");
+    // }
+
+    // if/else
+
+    if (age >= 18 && age < 65) {
+        print("Entrada Inteira permitida");
+    } else if (age >= 65) {
+        print("Meia entrada");
+    } else {
+        print("Tchau!");
+    }
+}
