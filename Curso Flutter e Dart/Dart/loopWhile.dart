195d6a5cb8aaafd4ccff3b7d6833ef19dfc701da
BRADDTELES <braddteles@gmail.com>
2024-08-31 10:29:40 -0300

    Loop While

---
+++ Curso Flutter e Dart/Dart/loopWhile.dart
@@ -0,0 +1,10 @@
+void main() {
+  // while laços
+
+  String texto = "Colocar um texto qualquer";
+  int i = 0;
+  while (i < texto.length) {
+    print(texto[i]);  // Acessando caracteres do texto usando colchetes [] e o método length() para determinar o comprimento do texto.
+    i++;
+  }
+}
