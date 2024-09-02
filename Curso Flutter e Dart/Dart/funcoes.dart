19f7a3d0c3aea0311eaf8dcb1109c78cdac809f5
BRADDTELES <braddteles@gmail.com>
2024-09-02 07:09:11 -0300

    Funções, aula boa de entender

---
+++ Curso Flutter e Dart/Dart/funcoes.dart
@@ -0,0 +1,26 @@
+void main() {
+  // functions
+  escreverMelhroCanal();
+  print(getMelhorCanal());
+  print(getNumero());
+
+  var pegueifuncao = getFlutuante();
+  print(pegueifuncao);
+  print(pegueifuncao.runtimeType);
+}
+
+void escreverMelhroCanal() {
+  print("WebDutras");
+}
+
+String getMelhorCanal() {
+  return "WebDutras";
+}
+
+int getNumero() {
+  return 150;
+}
+
+double getFlutuante() {
+  return 150.0;
+}
