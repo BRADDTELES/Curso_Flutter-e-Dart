bd06b3b7b42d4d8ce2855feb0623e1924726e787
BRADDTELES <braddteles@gmail.com>
2024-09-03 13:36:49 -0300

    Construtor parãmentros nomeados

---
+++ Curso Flutter e Dart/Dart/construtorParametrosNomeados.dart
@@ -0,0 +1,37 @@
+void main() {
+  
+  final carro = new Carro(
+    cor: 'vermelho', 
+    tamanho: 4.15, 
+    modelo: 'Gol'
+  );
+
+  print("Modelo: ${carro.modelo}");
+  print("Cor: ${carro.cor}");
+  print("Tamanho: ${carro.tamanho}m");
+  carro.ligar();
+  print('Andando: ${carro.isAndando()}');
+
+}
+
+class Carro {
+
+  final String cor;
+  final double tamanho;
+  final String modelo;
+
+  Carro({ 
+    required this.cor, 
+    required this.tamanho, 
+    required this.modelo 
+  });
+
+  void ligar() {
+    print('liguei');
+  }
+
+  bool isAndando() {
+    return false;
+  }
+
+}
