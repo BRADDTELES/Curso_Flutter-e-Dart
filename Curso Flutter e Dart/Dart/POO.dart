71b3d880a0620b30e51d789ce11c7b6a3ac74c25
BRADDTELES <braddteles@gmail.com>
2024-09-04 09:45:12 -0300

    POO em dart, inicializando conceitos

---
+++ Curso Flutter e Dart/Dart/POO.dart
@@ -0,0 +1,9 @@
+void main() {
+  /* 
+  * POO são 4 conceitos: Polimorfismo, Abastração, Herança e o Emcapsulamento
+  * DateTime é uma Classe
+  * now() é um método estático
+  */
+  var data = DateTime.now();
+  print(data);
+}
