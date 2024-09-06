30f2da85b56a9a7299c98bc227fbf5ed86cb43a2
BRADDTELES <braddteles@gmail.com>
2024-09-06 07:07:32 -0300

    Lists, aulda muito intensa de aprendizado

---
+++ Curso Flutter e Dart/Dart/lists.dart
@@ -0,0 +1,37 @@
+void main() {
+  List<String> listNomes = ["João", "Maria", "Bia"];
+  // print(listNomes[0]);
+  List<Estudante> listEstudantes = [
+    Estudante("Ewerton", 20),
+    Estudante("Maria", 25),
+    Estudante("João", 20),
+  ];
+
+  // final listFiltrada = [];
+
+  // for(final estudante in listEstudantes) {
+  //   if(estudante.idade <= 20) {
+  //     listFiltrada.add(estudante);
+  //   }
+  // }
+  // print(listFiltrada);
+
+
+  final listFiltrada = listEstudantes.where((estudante) => estudante.idade <= 20).toList();
+  // print(listFiltrada);
+  listEstudantes.add(Estudante("Bia", 18));
+  print(listEstudantes);
+  listEstudantes.removeAt(1);
+  print(listEstudantes);
+  
+}
+
+class Estudante {
+  String nome;
+  int idade;
+
+  Estudante(this.nome, this.idade);
+
+  @override
+  String toString() => "$nome";
+}
