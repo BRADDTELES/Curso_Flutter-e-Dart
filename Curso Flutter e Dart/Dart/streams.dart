3d166365970ccdfe93e05612c4956ba6c0164b3b
BRADDTELES <braddteles@gmail.com>
2024-09-10 07:12:41 -0300

    Streams, realizando o teste de pesquisa e implementação do desafio

--- Curso Flutter e Dart/Dart/streams.dart
+++ Curso Flutter e Dart/Dart/streams.dart
@@ -1,16 +1,20 @@
 void main() {
   // streams
-  contaInscricao().listen((val){
-    print(val);
-  }, onDone: () {
-    print("Terminamos de escutar");
-  });
-  print("Cheguei no final");
+  // contaInscricao().listen((val){
+  //   print(val);
+  // }, onDone: () {
+  //   print("Terminamos de escutar");
+  // });
+  // print("Cheguei no final");
+
+  stream.forEach(print);
 }
 
-Stream<int> contaInscricao() async* {
-  for (int i = 1; i <= 10; i++) {
-    yield i;
-    await Future.delayed(const Duration(seconds: 1));
-  }
-}
+// Stream<int> contaInscricao() async* {
+//   for (int i = 1; i <= 10; i++) {
+//     yield i;
+//     await Future.delayed(const Duration(seconds: 1));
+//   }
+// }
+
+final stream = Stream<int>.periodic(const Duration(seconds: 1), (count) => count * count).take(5);
