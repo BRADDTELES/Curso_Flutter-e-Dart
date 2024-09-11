a0d42764ca56f3fc0c61a7a12c694294c43a88c9
BRADDTELES <braddteles@gmail.com>
2024-09-11 10:53:17 -0300

    Futures (Promisses JS), inicio ft. 2

--- Curso Flutter e Dart/Dart/futuresPromisses.dart
+++ Curso Flutter e Dart/Dart/futuresPromisses.dart
@@ -1,16 +1,23 @@
+
+
 void main() async {
   /* Future (Promises JS) */
-  Future myFuture = Future(() {
-    print("estamos dentro de nossa função do futuro");
-    return 15;
-  }).then((val) => print("retorno: $val"));
-  print(myFuture);
-  // minhaFuncao().then((val) => print(val));
-  final res = await minhaFuncao();
-  print(res);
+  // Future myFuture = Future(() {
+  //   print("estamos dentro de nossa função do futuro");
+  //   return 15;
+  // }).then((val) => print("retorno: $val"));
+  // print(myFuture);
+  // // minhaFuncao().then((val) => print(val));
+  // final res = await minhaFuncao();
+  // print(res);
 
-  print("chegamos ao final");
+  // print("chegamos ao final");
+
+  
+
 }
+
+
 
 Future<int> minhaFuncao() async {
   print("Estou dentro função externa");
