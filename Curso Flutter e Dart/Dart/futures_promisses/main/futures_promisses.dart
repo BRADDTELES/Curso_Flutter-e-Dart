0ba77eccc9e1f2c12e15fb01443d5df0d50a6e0c
BRADDTELES <braddteles@gmail.com>
2024-09-11 16:30:26 -0300

    Buscando informações de um arquivo JSON

--- Curso Flutter e Dart/Dart/futures_promisses/main/futures_promisses.dart
+++ Curso Flutter e Dart/Dart/futures_promisses/main/futures_promisses.dart
@@ -1,10 +1,15 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() async {
-  var url = Uri.https("jsonplaceholder.typicode.com", '/users');
+  var url = Uri.https("jsonplaceholder.typicode.com", '/users/1');
 
   final res = await http.get(url);
-  print(res.statusCode);
+  print("Status: ${res.statusCode}");
+  String nome = jsonDecode(res.body)['name'];
+  String sobrenome = jsonDecode(res.body)['username'];
+  print(
+      "ID: ${jsonDecode(res.body)['id']}, Nome Completo: $nome $sobrenome, mora na cidade: ${jsonDecode(res.body)['address']['city']} e trabalha na Empressa: ${jsonDecode(res.body)['company']['catchPhrase']}");
 }
 
 void get() {
@@ -13,6 +18,6 @@ void get() {
 
 Future<int> minhaFuncao() async {
   print("Estou dentro função externa");
-  await Future.delayed(Duration(seconds: 3));
+  await Future.delayed(const Duration(seconds: 3));
   return 5;
 }
