20f3d8d467359e8a273896d7aa92e2c71da7063b
BRADDTELES <braddteles@gmail.com>
2024-09-11 15:26:20 -0300

    Corrigindo o arquivo Futures (Promisses JS), pois acontecia um erro de não instalação de pacote http. A correção foi ter criado outro projeto no modo Server app, aonde contem pastas e arquivos com dependencias, pacotes. '

--- Curso Flutter e Dart/Dart/futuresPromisses.dart
+++ Curso Flutter e Dart/Dart/futuresPromisses.dart
@@ -1,19 +1,16 @@
-
-
 void main() async {
   /* Future (Promises JS) */
-  // Future myFuture = Future(() {
-  //   print("estamos dentro de nossa função do futuro");
-  //   return 15;
-  // }).then((val) => print("retorno: $val"));
-  // print(myFuture);
-  // // minhaFuncao().then((val) => print(val));
-  // final res = await minhaFuncao();
-  // print(res);
-
-  // print("chegamos ao final");
-
+  Future myFuture = Future(() {
+    print("estamos dentro de nossa função do futuro");
+    return 15;
+  }).then((val) => print("retorno: $val"));
   
+  print(myFuture);
+  // minhaFuncao().then((val) => print(val));
+  final res = await minhaFuncao();
+  print(res);
+
+  print("chegamos ao final");
 
 }
 
