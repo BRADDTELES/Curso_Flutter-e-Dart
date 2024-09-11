94f26199caaebf941e56207b0b8680e86f7c7486
BRADDTELES <braddteles@gmail.com>
2024-09-11 16:56:55 -0300

    Adicionando os arquivos pubspec.yaml e pubspec.lock com os comandos pré definidos corretamente das dependencias, para o arquivo rodar o import http dentro do futuresPromisses.dart

--- Curso Flutter e Dart/Dart/futuresPromisses.dart
+++ Curso Flutter e Dart/Dart/futuresPromisses.dart
@@ -1,16 +1,38 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 void main() async {
   /* Future (Promises JS) */
-  Future myFuture = Future(() {
-    print("estamos dentro de nossa função do futuro");
-    return 15;
-  }).then((val) => print("retorno: $val"));
   
-  print(myFuture);
+  // Future myFuture = Future(() {
+  //   print("estamos dentro de nossa função do futuro");
+  //   return 15;
+  // }).then((val) => print("retorno: $val"));
+  
+  // print(myFuture);
   // minhaFuncao().then((val) => print(val));
-  final res = await minhaFuncao();
-  print(res);
+  // final res = await minhaFuncao();
+  // print(res);
+  // print("chegamos ao final");
 
-  print("chegamos ao final");
+  var url = Uri.https("jsonplaceholder.typicode.com", '/users/1');
+  http.get(url).then((value) {
+    print(jsonDecode(value.body)['name']);
+  }).catchError((error) { print("um erro aconteceu"); });
+  // try {
+  //   final res = await http.get(url);
+  //   print(jsonDecode(res.body)['id']);
+  // } catch (e) {
+  //   print("aconteceu um error");
+  // }
+
+  // final res = await http.get(url);
+  // print("Status: ${res.statusCode}");
+  // String nome = jsonDecode(res.body)['name'];
+  // String sobrenome = jsonDecode(res.body)['username'];
+  // print(
+  //     "ID: ${jsonDecode(res.body)['id']}, Nome Completo: $nome $sobrenome, mora na cidade: ${jsonDecode(res.body)['address']['city']} e trabalha na Empressa: ${jsonDecode(res.body)['company']['catchPhrase']}");
+  print('Acabou');
 
 }
 
