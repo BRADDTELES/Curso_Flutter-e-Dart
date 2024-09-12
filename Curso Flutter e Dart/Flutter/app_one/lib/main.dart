24a630b048f2f1beb9e6dfaba6cf4b0baec813ea
BRADDTELES <braddteles@gmail.com>
2024-09-12 17:21:34 -0300

    Função e correçaõ de explicação do rumApp em Flutter

--- Curso Flutter e Dart/Flutter/app_one/lib/main.dart
+++ Curso Flutter e Dart/Flutter/app_one/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() { /* o void main() é o ponto de partida do Dart */
-  runApp(const MyApp()); // rumApp ou o MyApp é o ponto de partida do Flutter
+  runApp(const MyApp()); // rumApp é o ponto de partida do Flutter
 }
