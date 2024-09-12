17a8ff36aa1f2d704db23410c3c0fbd29ca2d305
BRADDTELES <braddteles@gmail.com>
2024-09-12 18:51:46 -0300

    ft.4

---
+++ Curso Flutter e Dart/Flutter/first_app_flutter_no_vscode/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/usr/bin/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/danillo/Documentos/Curso_Flutter-e-Dart/Curso Flutter e Dart/Flutter/first_app_flutter_no_vscode" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/usr/bin/flutter"
+  "PROJECT_DIR=/home/danillo/Documentos/Curso_Flutter-e-Dart/Curso Flutter e Dart/Flutter/first_app_flutter_no_vscode"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/danillo/Documentos/Curso_Flutter-e-Dart/Curso Flutter e Dart/Flutter/first_app_flutter_no_vscode/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/danillo/Documentos/Curso_Flutter-e-Dart/Curso Flutter e Dart/Flutter/first_app_flutter_no_vscode/lib/main.dart"
+)
