c9e7e9f4c2307d1317f8b1331e8c2a1ddcf7426d
BRADDTELES <braddteles@gmail.com>
2024-09-13 10:05:20 -0300

    SE OCORRER ERRO: é só fazer o Pub get

--- Curso Flutter e Dart/Flutter/app_one/lib/main.dart
+++ Curso Flutter e Dart/Flutter/app_one/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() { /* o void main() é o ponto de partida do Dart */
-  runApp(const MyApp()); // rumApp é o ponto de partida do Flutter
+  runApp(Text("Olá, mundo", textDirection: TextDirection.ltr)); // rumApp é o ponto de partida do Flutter
 }
