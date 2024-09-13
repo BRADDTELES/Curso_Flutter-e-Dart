ce718c49954b61737f7d2e2ed93938b69f90134f
BRADDTELES <braddteles@gmail.com>
2024-09-13 15:04:24 -0300

    O que é Scaffold em Flutter?

--- Curso Flutter e Dart/Flutter/app_one/lib/main.dart
+++ Curso Flutter e Dart/Flutter/app_one/lib/main.dart
@@ -16,10 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Text(
-        "texto aqui antes Olá, mundo",
-      ),
-    );
+        home: Scaffold(
+      body: Text("Olá, Mundo!"),
+    ));
   }
 }
