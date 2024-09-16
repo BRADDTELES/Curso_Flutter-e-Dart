16262e4f88fb1e87995e1174a5d60eaa316babd4
BRADDTELES <braddteles@gmail.com>
2024-09-16 16:14:35 -0300

    Adicionando Tutorial de integração do Firebase no Flutter e o projeto de teste (second-web-evolui)

--- Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
@@ -7,10 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  double largura = 100;
-  double altura = 100;
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -21,32 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: largura,
-              height: altura,
-            ),
-            // SizedBox(
-            //   width: 50,
-            // ),
-            Container(
-              color: Colors.blue,
-              width: largura,
-              height: altura,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: largura,
-              height: altura,
-            )
-          ],
-        ),
-      ),
+      home: Container(),
+
     );
   }
 }
