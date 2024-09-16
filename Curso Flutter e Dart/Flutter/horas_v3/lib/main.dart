c5c2513c57253bca5406e89cbd35b5020a2e7d06
BRADDTELES <braddteles@gmail.com>
2024-09-16 17:39:30 -0300

    Criando a tela de login

--- Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:horas_v3/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: LoginScreen(),
 
     );
   }
--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  children: [
+                    FlutterLogo(size: 76),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(hintText: 'E-mail'),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      obscureText: true,
+                      controller: _senhaController,
+                      decoration: InputDecoration(hintText: 'Senha'),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Entrar com google'),
+                    ),
+                    SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Ainda não tem uma conta, crie uma conta'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
