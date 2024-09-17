356ced2f6712054ad3ba2ce55ebff1c03e8c0dd8
BRADDTELES <braddteles@gmail.com>
2024-09-17 16:01:23 -0300

    Criando nosso projeto no firebase e configurando ele #02

--- Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:horas_v3/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
