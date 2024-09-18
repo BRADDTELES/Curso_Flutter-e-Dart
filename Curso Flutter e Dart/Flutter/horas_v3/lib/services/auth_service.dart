3d3a623703632076d26071b3419da79eae4b7ee4
BRADDTELES <braddteles@gmail.com>
2024-09-18 09:46:56 -0300

    Criando nossa classe auth_service, autenticando Email e Senha; se usuario já existe

---
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/services/auth_service.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String?> entrarUsuario(
+      {required String email, required String senha}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: senha);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'user-not-found':
+          return 'Usuário não encontrado';
+        case 'wrong-password':
+          return 'Senha incorreta';
+      }
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> cadastrarUsuario(
+      {required String email,
+      required String senha,
+      required String nome}) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: senha);
+      await userCredential.user!.updateDisplayName(nome);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          return 'O email já está em uso';
+      }
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> redefinicaoSenha({required String email}) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'user-not-found':
+          return 'Usuário não encontrado';
+      }
+      return e.code;
+    }
+
+    return null;
+  }
+}
