6ad8141b4a62d2379f84b1b31c45c87a152b7e03
BRADDTELES <braddteles@gmail.com>
2024-09-18 17:02:53 -0300

    Roteador de telas

--- Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:horas_v3/screens/home_screen.dart';
 import 'package:horas_v3/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -26,8 +28,30 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: const RoteadorTelas(),
 
     );
   }
 }
+
+class RoteadorTelas extends StatelessWidget {
+  const RoteadorTelas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: FirebaseAuth.instance.userChanges(), builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(
+            child: CircularProgressIndicator()
+        );
+      } else {
+        if (snapshot.hasData) {
+          return HomeScreen(user: snapshot.data!);
+        } else {
+          return LoginScreen();
+        }
+      }
+    });
+  }
+}
+
