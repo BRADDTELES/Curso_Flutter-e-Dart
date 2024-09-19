a5125a1760f763b0c9e0443b83e26deceac3f834
BRADDTELES <braddteles@gmail.com>
2024-09-19 15:00:39 -0300

    Quase finalizando a tela HomeScreen

--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/home_screen.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/home_screen.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:horas_v3/components/menu.dart';
 import 'package:horas_v3/helpers/hour_helpers.dart';
 import 'package:horas_v3/models/hour.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class HomeScreen extends StatefulWidget {
   final User user;
 
-  HomeScreen({super.key, required this.user});
+  const HomeScreen({super.key, required this.user});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -28,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       drawer: Menu(user: widget.user),
       appBar: AppBar(
-        title: Text('Horas V3'),
+        title: const Text('Horas V3'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -43,7 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             )
           : ListView(
-              padding: EdgeInsets.only(left: 4, right: 4),
+              padding: const EdgeInsets.only(left: 4, right: 4),
               children: List.generate(listHours.length, (index) {
                 Hour model = listHours[index];
                 return Dismissible(
@@ -53,7 +54,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     alignment: Alignment.centerRight,
                     padding: const EdgeInsets.only(right: 12),
                     color: Colors.red,
-                    child: Icon(Icons.delete, color: Colors.white),
+                    child: const Icon(Icons.delete, color: Colors.white),
                   ),
                   onDismissed: (direction) {
                     remove(model);
@@ -65,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ListTile(
                           onLongPress: () {},
                           onTap: () {},
-                          leading: Icon(
+                          leading: const Icon(
                             Icons.list_alt_rounded,
                             size: 56,
                           ),
@@ -81,9 +82,105 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
     );
   }
-  void remove(Hour model) {
 
+  showFormModal({Hour? model}) {
+    String title = "Adicionar";
+    String confirmationButton = "Salvar";
+    String skipButton = "Cancelar";
+
+    TextEditingController dataController = TextEditingController();
+    final dataMaskFormatter = MaskTextInputFormatter(mask: '##/##/####');
+    TextEditingController minutosController = TextEditingController();
+    final minutosMaskFormatter = MaskTextInputFormatter(mask: '##:##');
+    TextEditingController descricaoController = TextEditingController();
+
+    if (model != null) {
+      title = "Editando";
+      dataController.text = model.data;
+      minutosController.text = HourHelper.minutesToHours(model.minutos);
+      if (model.descricao != null) {
+        descricaoController.text = model.descricao!;
+      }
+    }
+
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(24),
+        ),
+      ),
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(32),
+          child: ListView(
+            children: [
+              Text(title, style: Theme.of(context).textTheme.headlineSmall),
+              TextFormField(
+                controller: dataController,
+                keyboardType: TextInputType.datetime,
+                decoration: const InputDecoration(
+                  hintText: '01/01/2024',
+                  labelText: 'Data',
+                ),
+                inputFormatters: [dataMaskFormatter],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: minutosController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: '00:00',
+                  labelText: 'Minutos',
+                ),
+                inputFormatters: [minutosMaskFormatter],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: descricaoController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  hintText: 'Lembrete do que você fez',
+                  labelText: 'Descrição',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      skipButton,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(confirmationButton),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 180,
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
+
+  void remove(Hour model) {}
 }
-
-
