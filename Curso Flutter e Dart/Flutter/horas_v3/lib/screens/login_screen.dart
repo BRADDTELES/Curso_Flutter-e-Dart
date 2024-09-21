ffb1071b4660d8f3fc7e5d7190c554fa20f45c87
BRADDTELES <braddteles@gmail.com>
2024-09-21 20:13:14 -0300

    Primeira parte Já configurado

--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:horas_v3/screens/register_screen.dart';
 import 'package:horas_v3/services/auth_service.dart';
 import 'package:horas_v3/screens/reset_password_modal.dart';
@@ -61,7 +63,7 @@ class LoginScreen extends StatelessWidget {
                       const SizedBox(height: 16),
                       ElevatedButton(
                         onPressed: () {
-
+                          singinWithGoogle();
                         },
                         child: const Text('Entrar com google'),
                       ),
@@ -92,5 +94,15 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<UserCredential> singinWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
