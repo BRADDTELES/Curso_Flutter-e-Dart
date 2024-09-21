58e5974fd528d7854524638d0815e020a096d1a1
BRADDTELES <braddteles@gmail.com>
2024-09-21 09:20:59 -0300

    Atualizando a main

--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:horas_v3/screens/register_screen.dart';
-import 'package:horas_v3/services/auth_service.dart';
 import 'package:horas_v3/screens/reset_password_modal.dart';
+import 'package:horas_v3/services/auth_service.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -21,8 +21,7 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              // RenderFlex overflowed >CORREÇÃO> child: SingleChildScrollView(
+            child: Column( // RenderFlex overflowed >CORREÇÃO> child: SingleChildScrollView(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
@@ -48,23 +47,15 @@ class LoginScreen extends StatelessWidget {
                         decoration: const InputDecoration(hintText: 'Senha'),
                       ),
                       const SizedBox(height: 16),
-                      ElevatedButton(
-                          onPressed: () {
-                            authService
-                                .entrarUsuario(
-                                    email: _emailController.text,
-                                    senha: _senhaController.text)
-                                .then((String? erro) {
-                              if (erro != null) {
-                                final snackBar = SnackBar(
-                                    content: Text(erro),
-                                    backgroundColor: Colors.red);
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(snackBar);
-                              }
-                            });
-                          },
-                          child: const Text('Entrar')),
+                      ElevatedButton(onPressed: () {
+                        authService.entrarUsuario(email: _emailController.text, senha: _senhaController.text).then((String? erro) {
+                          if (erro != null) {
+                            final snackBar = SnackBar(content: Text(erro),
+                              backgroundColor: Colors.red);
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          }
+                        });
+                      }, child: const Text('Entrar')),
                       const SizedBox(height: 16),
                       ElevatedButton(
                         onPressed: () {
@@ -75,14 +66,10 @@ class LoginScreen extends StatelessWidget {
                       const SizedBox(height: 16),
                       TextButton(
                         onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => RegisterScreen(),
-                              ));
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen(),
+                          ));
                         },
-                        child: const Text(
-                            'Ainda não tem uma conta, crie uma conta'),
+                        child: const Text('Ainda não tem uma conta, crie uma conta'),
                       ),
                       TextButton(
                         onPressed: () {
--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/reset_password_modal.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/reset_password_modal.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:horas_v3/services/auth_service.dart';
 
-class PasswordresetModal extends StatefulWidget {
-  const PasswordresetModal({super.key});
+class PasswordResetModal extends StatefulWidget {
+  const PasswordResetModal({super.key});
 
   @override
-  State<PasswordresetModal> createState() => _PasswordresetModalState();
+  State<PasswordResetModal> createState() => _PasswordResetModalState();
 }
 
-class _PasswordresetModalState extends State<PasswordresetModal> {
+class _PasswordResetModalState extends State<PasswordResetModal> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
 
   AuthService authService = AuthService();
-
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Recuperar senha'),
+      title: Text("Recuperar senha"),
       content: Form(
         key: _formKey,
         child: TextFormField(
@@ -26,43 +25,12 @@ class _PasswordresetModalState extends State<PasswordresetModal> {
           decoration: const InputDecoration(labelText: 'Endereço de e-mail'),
           validator: (value) {
             if (value!.isEmpty) {
-              return 'Informe um endereço de e-mail válido';
+              return 'Por favor, informe um endereço de e-mail válido';
             }
             return null;
           },
         ),
       ),
-      actions: <TextButton>[
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Cancelar'),
-        ),
-        TextButton(
-          onPressed: () {
-            if (_formKey.currentState!.validate()) {
-              authService
-                  .redefinicaoSenha(email: _emailController.text)
-                  .then((String? erro) {
-                Navigator.of(context).pop();
-                if (erro != null) {
-                  final snackBar = SnackBar(
-                      content: Text(erro), backgroundColor: Colors.red);
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                } else {
-                  final snackBar = SnackBar(
-                    content: Text(
-                        'Um link de redefinição de senha foi enviado para o seu e-mail: ${_emailController.text}'),
-                      backgroundColor: Colors.green);
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              });
-            }
-          },
-          child: Text('Recuperar senha'),
-        )
-      ],
     );
   }
 }
