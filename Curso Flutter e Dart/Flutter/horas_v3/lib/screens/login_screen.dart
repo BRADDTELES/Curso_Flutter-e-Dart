5191d2d4cf7f96c17be3cc628c1dbdadac8d4a25
BRADDTELES <braddteles@gmail.com>
2024-09-22 14:47:51 -0300

    atualizando a main

--- Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
+++ Curso Flutter e Dart/Flutter/horas_v3/lib/screens/login_screen.dart
@@ -21,7 +21,8 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
-            child: Column( // RenderFlex overflowed >CORREÇÃO> child: SingleChildScrollView(
+            child: Column(
+              // RenderFlex overflowed >CORREÇÃO> child: SingleChildScrollView(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
@@ -47,15 +48,23 @@ class LoginScreen extends StatelessWidget {
                         decoration: const InputDecoration(hintText: 'Senha'),
                       ),
                       const SizedBox(height: 16),
-                      ElevatedButton(onPressed: () {
-                        authService.entrarUsuario(email: _emailController.text, senha: _senhaController.text).then((String? erro) {
-                          if (erro != null) {
-                            final snackBar = SnackBar(content: Text(erro),
-                              backgroundColor: Colors.red);
-                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                          }
-                        });
-                      }, child: const Text('Entrar')),
+                      ElevatedButton(
+                          onPressed: () {
+                            authService
+                                .entrarUsuario(
+                                    email: _emailController.text,
+                                    senha: _senhaController.text)
+                                .then((String? erro) {
+                              if (erro != null) {
+                                final snackBar = SnackBar(
+                                    content: Text(erro),
+                                    backgroundColor: Colors.red);
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              }
+                            });
+                          },
+                          child: const Text('Entrar')),
                       const SizedBox(height: 16),
                       ElevatedButton(
                         onPressed: () {
@@ -66,19 +75,24 @@ class LoginScreen extends StatelessWidget {
                       const SizedBox(height: 16),
                       TextButton(
                         onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen(),
-                          ));
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RegisterScreen(),
+                              ));
                         },
-                        child: const Text('Ainda não tem uma conta, crie uma conta'),
+                        child: const Text(
+                            'Ainda não tem uma conta, crie uma conta'),
                       ),
                       TextButton(
                           onPressed: () {
-                            showDialog(context: context, builder: (BuildContext context) {
-                              return PasswordResetModal();
-                            });
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return const PasswordResetModal();
+                                });
                           },
-                          child: Text("Esqueceu sua senha")
-                      ),
+                          child: const Text("Esqueceu sua senha")),
                     ],
                   ),
                 )
@@ -92,7 +106,8 @@ class LoginScreen extends StatelessWidget {
 
   Future<UserCredential> singinWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
@@ -101,5 +116,3 @@ class LoginScreen extends StatelessWidget {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
-
-
